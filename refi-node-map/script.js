332a45e1d674c14c6a2a2b35995a989a80724944
MontyMerlin <monty@montybryant.co.uk>
2026-02-17 17:13:41 +0100

    Network page + new What is Regeneration? section

--- refi-node-map/script.js
+++ refi-node-map/script.js
@@ -667,6 +667,7 @@ function nodePoint(node) {
                     <button class="pin" type="button" data-node-id="${escapeHtml(node.id)}" style="left:${xPct}%; top:${yPct}%;">
                         <span class="pin__dot" aria-hidden="true"></span>
                         <div class="pin__bubble" aria-hidden="true">
+                            <img class="pin__cover" alt="" loading="lazy" hidden />
                             <div class="pin__title">
                                 <span class="pin__flag" aria-hidden="true">${escapeHtml(flag)}</span>
                                 <span class="pin__label">${escapeHtml(node.name)}</span>
@@ -690,6 +691,14 @@ function nodePoint(node) {
                 `;
             })
             .join("");
+        mapPins.querySelectorAll(".pin").forEach((pin) => {
+            const nodeId = pin.dataset.nodeId;
+            const node = nodes.find((item) => item.id === nodeId);
+            const img = pin.querySelector(".pin__cover");
+            if (node && img) {
+                setCoverImage(img, node);
+            }
+        });
     }
 
     function sortNodes(list) {
